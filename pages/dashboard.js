37c8172fd67dd815312f15f338504154d6dcc28d
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-02-03 09:43:51 +0400

    Dashboard version mobile et laptop OK

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -15,6 +15,8 @@ import ArticleIcon from "@mui/icons-material/Article";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
 import DeleteIcon from "@mui/icons-material/Delete";
+import InfoIcon from "@mui/icons-material/Info";
+import CircleIcon from '@mui/icons-material/Circle';
 
 const Dashboard = () => {
   // va me permettre de toggle entre le theme clair et sombre
@@ -336,6 +338,24 @@ const Dashboard = () => {
                 </p>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div
+          className={
+            lightMode ? `${styles.news}` : `${styles.news} ${styles.dark}`
+          }
+        >
+          <div className={styles.news__top}>
+            <h2 className={lightMode ? `${styles.news__top__h2}` : `${styles.news__top__h2} ${styles.dark}`}>Linkedin News</h2>
+            <InfoIcon className={lightMode ? `${styles.news__top__icon}` : `${styles.news__top__icon} ${styles.dark}`}/>
+          </div>
+          <div className={styles.news__items}>
+            <div className={styles.news__items__title}>
+                <CircleIcon className={lightMode ? `${styles.news__items__title__icon}` : `${styles.news__items__title__icon} ${styles.dark}`}/>
+                <p className={lightMode ? `${styles.news__items__title__para}` : `${styles.news__items__title__para} ${styles.dark}`}>Voila une superbe news !</p>
+            </div>
+            <p className={styles.news__items__time}>Il y a 14 minutes</p>
           </div>
         </div>
       </main>
