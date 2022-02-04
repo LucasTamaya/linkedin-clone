801a10d9a941c41c100be25e408391e21bc4d6e7
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-02-04 23:33:43 +0400

    correction cors error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const template = require("./helpers/template")
+
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${template}:path*`,
+      },
+    ]
+  },
 };
