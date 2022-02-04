e2acefe6e10fc67522d4743ce9206d5f123ef1b9
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-02-04 11:46:06 +0400

    Dashboard left finit

--- components/DashboardUser.js
+++ components/DashboardUser.js
@@ -5,121 +5,149 @@ import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
 
 const DashboardUser = ({ lightMode }) => {
   return (
-    <div
-      className={
-        lightMode ? `${styles.userCard}` : `${styles.userCard} ${styles.dark}`
-      }
-    >
+    <div className={styles.dashboardLeft}>
       <div
         className={
-          lightMode
-            ? `${styles.userCard__contact}`
-            : `${styles.userCard__contact} ${styles.dark}`
+          lightMode ? `${styles.userCard}` : `${styles.userCard} ${styles.dark}`
         }
       >
-        <FaceIcon
+        <div
           className={
             lightMode
-              ? `${styles.userCard__icon}`
-              : `${styles.userCard__icon} ${styles.dark}`
+              ? `${styles.userCard__contact}`
+              : `${styles.userCard__contact} ${styles.dark}`
           }
-        />
+        >
+          <FaceIcon
+            className={
+              lightMode
+                ? `${styles.userCard__icon}`
+                : `${styles.userCard__icon} ${styles.dark}`
+            }
+          />
 
-        <div className={styles.userCard__contactContainer}>
+          <div className={styles.userCard__contactContainer}>
+            <p
+              className={
+                lightMode
+                  ? `${styles.userCard__contactContainer__name}`
+                  : `${styles.userCard__contactContainer__name} ${styles.dark}`
+              }
+            >
+              Lucas T
+            </p>{" "}
+            {/*Récupérer le nom entré lors du login / register*/}
+            <p className={styles.userCard__contactContainer__email}>
+              lucas.tamaya@orange.fr
+            </p>{" "}
+            {/*Récupérer le mail entré lors du login / register*/}
+          </div>
+        </div>
+
+        <div className={styles.separationLine}></div>
+
+        <div className={styles.userCard__views}>
+          <div className={styles.userCard__viewsContainer}>
+            <h3
+              className={
+                lightMode
+                  ? `${styles.userCard__viewsContainer__h3}`
+                  : `${styles.userCard__viewsContainer__h3} ${styles.dark}`
+              }
+            >
+              Who viewed your profile
+            </h3>
+            <p className={styles.userCard__viewsContainer__number}>16</p>
+          </div>
+          <div className={styles.userCard__viewsContainer}>
+            <h3
+              className={
+                lightMode
+                  ? `${styles.userCard__viewsContainer__h3}`
+                  : `${styles.userCard__viewsContainer__h3} ${styles.dark}`
+              }
+            >
+              Views of your posts
+            </h3>
+            <p className={styles.userCard__viewsContainer__number}>100</p>
+          </div>
+        </div>
+
+        <div className={styles.separationLine}></div>
+
+        <div className={styles.userCard__premium}>
           <p
             className={
               lightMode
-                ? `${styles.userCard__contactContainer__name}`
-                : `${styles.userCard__contactContainer__name} ${styles.dark}`
+                ? `${styles.userCard__premium__para}`
+                : `${styles.userCard__premium__para} ${styles.dark}`
             }
           >
-            Lucas T
-          </p>{" "}
-          {/*Récupérer le nom entré lors du login / register*/}
-          <p className={styles.userCard__contactContainer__email}>
-            lucas.tamaya@orange.fr
-          </p>{" "}
-          {/*Récupérer le mail entré lors du login / register*/}
+            {" "}
+            Access exclusive tools and insights
+          </p>
+          <div className={styles.userCard__premiumContainer}>
+            <WorkspacePremiumIcon
+              className={styles.userCard__premiumContainer__icon}
+            />
+            <h3
+              className={
+                lightMode
+                  ? `${styles.userCard__premiumContainer__h3}`
+                  : `${styles.userCard__premiumContainer__h3} ${styles.dark}`
+              }
+            >
+              Try Premium for free
+            </h3>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.separationLine}></div>
+        <div className={styles.separationLine}></div>
 
-      <div className={styles.userCard__views}>
-        <div className={styles.userCard__viewsContainer}>
-          <h3
+        <div className={styles.userCard__items}>
+          <TurnedInIcon
             className={
               lightMode
-                ? `${styles.userCard__viewsContainer__h3}`
-                : `${styles.userCard__viewsContainer__h3} ${styles.dark}`
+                ? `${styles.userCard__items__icon}`
+                : `${styles.userCard__items__icon} ${styles.dark}`
             }
-          >
-            Who viewed your profile
-          </h3>
-          <p className={styles.userCard__viewsContainer__number}>16</p>
-        </div>
-        <div className={styles.userCard__viewsContainer}>
-          <h3
-            className={
-              lightMode
-                ? `${styles.userCard__viewsContainer__h3}`
-                : `${styles.userCard__viewsContainer__h3} ${styles.dark}`
-            }
-          >
-            Views of your posts
-          </h3>
-          <p className={styles.userCard__viewsContainer__number}>100</p>
-        </div>
-      </div>
-
-      <div className={styles.separationLine}></div>
-
-      <div className={styles.userCard__premium}>
-        <p
-          className={
-            lightMode
-              ? `${styles.userCard__premium__para}`
-              : `${styles.userCard__premium__para} ${styles.dark}`
-          }
-        >
-          {" "}
-          Access exclusive tools and insights
-        </p>
-        <div className={styles.userCard__premiumContainer}>
-          <WorkspacePremiumIcon
-            className={styles.userCard__premiumContainer__icon}
           />
           <h3
             className={
               lightMode
-                ? `${styles.userCard__premiumContainer__h3}`
-                : `${styles.userCard__premiumContainer__h3} ${styles.dark}`
+                ? `${styles.userCard__items__h3}`
+                : `${styles.userCard__items__h3} ${styles.dark}`
             }
           >
-            Try Premium for free
+            My items
           </h3>
         </div>
       </div>
 
-      <div className={styles.separationLine}></div>
+      <div
+        className={
+          lightMode
+            ? `${styles.discoverCard}`
+            : `${styles.discoverCard} ${styles.dark}`
+        }
+      >
+        <div className={styles.discoverCard__linksContainer}>
+          <p className={styles.discoverCard__linksContainer__link}>Groups</p>
+          <p className={styles.discoverCard__linksContainer__link}>Events</p>
+          <p className={styles.discoverCard__linksContainer__link}>
+            Followed Hashtags
+          </p>
+        </div>
+        <div className={styles.separationLine}></div>
 
-      <div className={styles.userCard__items}>
-        <TurnedInIcon
-          className={
-            lightMode
-              ? `${styles.userCard__items__icon}`
-              : `${styles.userCard__items__icon} ${styles.dark}`
-          }
-        />
-        <h3
-          className={
-            lightMode
-              ? `${styles.userCard__items__h3}`
-              : `${styles.userCard__items__h3} ${styles.dark}`
-          }
-        >
-          My items
-        </h3>
+        <div className={styles.discoverCard__discoverMore}>
+          <h3 className={
+          lightMode
+            ? `${styles.discoverCard__discoverMore__h3}`
+            : `${styles.discoverCard__discoverMore__h3} ${styles.dark}`
+        }>Discover More</h3>
+        </div>
+
       </div>
     </div>
   );
